423278679a86da2ce50c4d3645ce1f6b6e9cce8d
rmtrane <rmtrane@gmail.com>
2020-11-16 14:08:33 -0600

    Updated power_sims.R with first look figure

--- power_sims.R
+++ power_sims.R
@@ -3,9 +3,10 @@ library(ACEBounds)
 library(distributions3)
 library(furrr)
 
-plan(multiprocess)
+plan(multicore)
 
-many_sims <- expand_grid(i = 1:10,
+for (i in 1:10){
+many_sims <- expand_grid(i = i,
                          indIVs_on_X = 1:10/15*2,
                          X_on_Y = c(0.5, 1:10)) %>%
   mutate(
@@ -24,16 +25,51 @@ many_sims <- expand_grid(i = 1:10,
     )
   )
 
-many_sims_w_bounds <- many_sims %>%
-  mutate(sum_stats = list(probs_from_data(sim_data$simulated_data, X, Y, Z1, data_format = "bivariate")),
-         get_bounds_res = list(get_bounds(gammas = sum_stats$gammas, thetas = sum_stats$thetas, stop = FALSE)),
-         bounds = list(get_bounds_res$interval))
+#many_sims_w_bounds <- many_sims %>%
+#  mutate(sum_stats = list(probs_from_data(sim_data$simulated_data, X, Y, Z1, data_format = "bivariate")),
+#         get_bounds_res = list(get_bounds(gammas = sum_stats$gammas, thetas = sum_stats$thetas, stop = FALSE)),
+#         bounds = list(get_bounds_res$interval))
 
-write_rds(many_sims, "sims_for_power.Rds")
-
+write_rds(many_sims, paste0("sims_for_power_", stringr::str_pad(i, width = 2, side = "left", pad = "0"),".Rds"))
+}
 # many_sims_w_bounds %>%
 #   unnest_wider(bounds) %>%
 #   arrange(desc(lower)) %>%
 #   ggplot(aes(x = indIVs_on_X)) +
 #     geom_errorbar(aes(ymin = lower, ymax = upper)) +
 #     facet_grid(~X_on_Y)
+
+
+
+all_sims <- tibble(sims_for_power = list.files(pattern = "sims_for_power")) %>%
+  rowwise() %>%
+  mutate(sims = list(read_rds(sims_for_power))) %>%
+  ungroup() %>%
+  unnest(sims)
+
+
+all_sims_w_bounds <- all_sims %>%
+  mutate(simulated_data = map(sim_data, "simulated_data"),
+         sum_stats = map(simulated_data, ~probs_from_data(.x, X, Y, Z1, data_format = "bivariate")),
+         get_bounds_res = map(sum_stats, ~get_bounds(gammas = .x$gammas, thetas = .x$thetas, stop = FALSE)),
+         bounds = map(get_bounds_res, "interval"))
+
+
+all_sims_w_bounds %>%
+  unnest_wider(bounds) %>%
+  mutate(zero = lower < 0 & upper > 0) %>%
+  ggplot(aes(x = i, color = zero)) +
+    geom_hline(yintercept = 0, color = "red", linetype = "dashed") +
+    geom_errorbar(aes(ymin = lower, ymax = upper)) +
+    facet_grid(X_on_Y ~ indIVs_on_X)
+
+
+all_sims_w_bounds %>%
+  unnest_wider(bounds) %>%
+  group_by(X_on_Y, indIVs_on_X) %>%
+  summarize(
+    s_lower = sd(lower),
+    s_upper = sd(upper),
+    lower_range = max(lower) - min(lower),
+    upper_range = max(upper) - min(upper)
+  )

027670aeda225adad2948e267e4a1db2523176a1
rmtrane <rmtrane@gmail.com>
2020-11-15 18:16:55 -0600

    Updated power_sims.R to save results

--- power_sims.R
+++ power_sims.R
@@ -5,13 +5,14 @@ library(furrr)
 
 plan(multiprocess)
 
-many_sims <- expand_grid(indIVs_on_X = 1:10/15*2,
+many_sims <- expand_grid(i = 1:10,
+                         indIVs_on_X = 1:10/15*2,
                          X_on_Y = c(0.5, 1:10)) %>%
   mutate(
     sim_data = future_map2(
       indIVs_on_X, X_on_Y,
       ~simulate_data(
-        sample_size = 10e6,
+        sample_size = 10e5,
         IVs_ps = list(c(0.25, 0.5, 0.25)),
         X_intercept = -1,
         indIVs_on_X = .x,
@@ -27,6 +28,8 @@ many_sims_w_bounds <- many_sims %>%
   mutate(sum_stats = list(probs_from_data(sim_data$simulated_data, X, Y, Z1, data_format = "bivariate")),
          get_bounds_res = list(get_bounds(gammas = sum_stats$gammas, thetas = sum_stats$thetas, stop = FALSE)),
          bounds = list(get_bounds_res$interval))
+
+write_rds(many_sims, "sims_for_power.Rds")
 
 # many_sims_w_bounds %>%
 #   unnest_wider(bounds) %>%

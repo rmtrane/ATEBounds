f579d0261066dfdc23d109b47c881dd8adb444f2
Ralph Trane <rmtrane@gmail.com>
2021-01-29 20:29:38 -0600

    Add results from SLURM

--- slurm/run_scripts/run_slurm_power_prep_for_plots.sh
+++ slurm/run_scripts/run_slurm_power_prep_for_plots.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp power_prep_for_plots.R /workspace/rtrane/ACEBounds/.
+cp ~/ACEBounds/scripts/power/power_prep_for_plots.R /workspace/rtrane/ACEBounds/scripts/power/.
 
-sbatch slurm_power_prep_for_plots.sh
+sbatch ~/ACEBounds/slurm/slurm_scripts/slurm_power_prep_for_plots.sh
--- slurm/slurm_scripts/slurm_power_sim.sh
+++ slurm/slurm_scripts/slurm_power_sim.sh
@@ -8,7 +8,7 @@
 #SBATCH -t 72:00:00
 #SBATCH -p long
 #SBATCH --mem-per-cpu=8000M
-#SBATCH --array=558 # 1-840
+#SBATCH --array=1-840
 
 # Make sure R knows where I keep my packages
 export R_LIBS=/workspace/rtrane/Rpackages
@@ -17,6 +17,6 @@ module load R/R-4.0.1
 
 mkdir V8_$SLURM_ARRAY_TASK_ID
 
-R CMD BATCH --no-save --no-restore "--args $SLURM_ARRAY_TASK_ID" power_sims_only.R power_sims_out/power_sims_$SLURM_ARRAY_TASK_ID.Rout
+R CMD BATCH --no-save --no-restore "--args $SLURM_ARRAY_TASK_ID" scripts/power/power_sims_only.R power_sims_out/power_sims_$SLURM_ARRAY_TASK_ID.Rout
 
 rm -rf V8_$SLURM_ARRAY_TASK_ID
